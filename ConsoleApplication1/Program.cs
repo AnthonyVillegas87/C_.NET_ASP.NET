73a5a0cc9d3db9869dc7b77772c4299e07346acd
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-18 01:06:45 -0500

    feat: xml file created

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -31,8 +31,53 @@ namespace  ConsoleApplication1
         public string TypeofDebtor;
         public long BarId;
         public bool IsActive;
+
+        private void DeserializeObject(string filename)
+        {
+            Console.WriteLine("Reading with XmlReader");
+            
+            //Create an instance of XmlSerializer specifying type
+            XmlSerializer serializer = new XmlSerializer(typeof(Program));
+
+            // To read our XML Document
+            FileStream fileStream = new FileStream(filename, FileMode.Open);
+            XmlReader reader = XmlReader.Create(fileStream);
+            
+            //Declare our object of the type to deserialize
+            Program debtor = (Program) serializer.Deserialize(reader);
+            fileStream.Close();
+            
+            //Write out our properties
+            Console.Write(
+                debtor.Debtor + "\t" +
+                debtor.DebtorSsn + "\t" +
+                debtor.DebtorAddress1 + "\t" +
+                debtor.DebtorAddress2 + "\t" +
+                debtor.DebtorAddress3 + "\t" +
+                debtor.DebtorCounty + "\t" +
+                debtor.DebtorEmail + "\t" +
+                debtor.DateOfBirth + "\t" +
+                debtor.DebtorCellPhone + "\t" +
+                debtor.Spouse + "\t" +
+                debtor.SpouseSsn + "\t" + 
+                debtor.SpouseAddress1 + "\t" +
+                debtor.SpouseAddress2 + "\t" + 
+                debtor.SpouseAddress3 + "\t" +
+                debtor.SpouseCounty + "\t" +
+                debtor.SpouseEmail + "\t" +
+                debtor.SpouseDateOfBirth + "\t" +
+                debtor.SpouseCellPhone + "\t" +
+                debtor.Chapter + "\t" +
+                debtor.TypeofDebtor + "\t" +
+                debtor.BarId + "\t" +
+                debtor.IsActive
+            );
+
+        }
         public static void Main(string[] args)
         {
+            Program app = new Program();
+            app.DeserializeObject("form2.xml");
         }
     }
     
