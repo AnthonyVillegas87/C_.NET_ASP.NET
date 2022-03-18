31b737aa13c3629071a1356bad8a30f31be9beb3
Anthony Villegas <villegasanthony87@yahoo.com>
2022-03-18 00:22:10 -0500

    class intro C#

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace  ConsoleApplication1
 {
-    internal class Program
+    
+    public class Program
     {
+        public string Debtor;
+        public string DebtorSsn;
+        public string DebtorAddress1;
+        public string DebtorAddress2;
+        public string DebtorAddress3;
+        public string DebtorCounty;
+        public string DebtorEmail;
+        public string DateOfBirth;
+        public int DebtorCellPhone;
+        public string Spouse;
+        public string SpouseSsn;
+        public string SpouseAddress1;
+        public string SpouseAddress2;
+        public string SpouseAddress3;
+        public string SpouseCounty;
+        public string SpouseEmail;
+        public string SpouseDateOfBirth;
+        public int SpouseCellPhone;
+        public int Chapter;
+        public string TypeofDebtor;
+        public long BarId;
+        public bool IsActive;
         public static void Main(string[] args)
         {
         }
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
